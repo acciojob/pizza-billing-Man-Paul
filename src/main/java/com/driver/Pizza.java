ca2f9b134965f0dac69c60080be87446dce93f8a
Manoj Paul <manojpaul111@gmail.com>
2023-03-06 22:33:12 +0530

    Pizza Billing - Assignment

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,9 +6,19 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    public int extraCheese = 0;
+
+    public int extraToppings = 0;
+
+    public int paperbag = 0;
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(this.isVeg == true)
+            price = 300;
+        else
+            price = 400;
     }
 
     public int getPrice(){
@@ -17,18 +27,28 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        extraCheese = 80;
     }
 
     public void addExtraToppings(){
         // your code goes here
+        extraToppings = 70;
     }
 
     public void addTakeaway(){
         // your code goes here
+        paperbag = 20;
     }
 
     public String getBill(){
         // your code goes here
+        bill = "";
+        int total = getPrice() + extraCheese + extraToppings + paperbag;
+        bill += "Base Price of the Pizza: " + getPrice() + "\n" +
+                "Extra Cheese Added: " + extraCheese + "\n" +
+                "Extra Toppings Added: " + extraToppings + "\n" +
+                "Paperbag Added: " + paperbag + "\n" +
+                "Total Price: " + total;
         return this.bill;
     }
 }
