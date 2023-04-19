95648a18f56ee90e0b70c9a308471304cca2199d
Manoj Paul <manojpaul111@gmail.com>
2023-04-20 01:43:10 +0530

    Pizza Assignment - update 1

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,7 +5,7 @@ public class Pizza {
     private int price;
     private int totalPrice;
     private Boolean isVeg;
-    private String bill = "";
+    public String bill = "";
     private int cheeseCount = 0;
     private int toppingsCount = 0;
     private int takeawayCount = 0;
@@ -15,13 +15,13 @@ public class Pizza {
         // your code goes here
         if(this.isVeg){
             this.price = 300;
-            totalPrice = 300;
+            this.totalPrice = 300;
         }
         else{
             this.price = 400;
-            totalPrice = 400;
+            this.totalPrice = 400;
         }
-        bill += "Base Price Of The Pizza: " + totalPrice + "\n";
+        this.bill += "Base Price Of The Pizza: " + this.totalPrice + "\n";
     }
 
     public int getPrice(){
@@ -31,8 +31,8 @@ public class Pizza {
     public void addExtraCheese(){
         // your code goes here
         if(cheeseCount == 0){
-            totalPrice += 80;
-            bill += "Extra Cheese Added: " + 80 + "\n";
+            this.totalPrice += 80;
+            this.bill += "Extra Cheese Added: " + 80 + "\n";
         }
         cheeseCount = 1;
     }
@@ -40,12 +40,12 @@ public class Pizza {
     public void addExtraToppings(){
         // your code goes here
         if(toppingsCount == 0 && this.getPrice() == 300){   //veg
-            totalPrice += 70;
-            bill += "Extra Toppings Added: " + 70 + "\n";
+            this.totalPrice += 70;
+            this.bill += "Extra Toppings Added: " + 70 + "\n";
         }
         else if(toppingsCount == 0 && this.getPrice() == 400){//non-veg
-            totalPrice += 120;
-            bill += "Extra Toppings Added: " + 120 + "\n";
+            this.totalPrice += 120;
+            this.bill += "Extra Toppings Added: " + 120 + "\n";
         }
         toppingsCount = 1;
     }
@@ -53,15 +53,15 @@ public class Pizza {
     public void addTakeaway(){
         // your code goes here
         if(takeawayCount == 0){
-            totalPrice += 20;
-            bill += "Paperbag Added: " + 20 + "\n";
+            this.totalPrice += 20;
+            this.bill += "Paperbag Added: " + 20 + "\n";
         }
         takeawayCount = 1;
     }
 
     public String getBill(){
         // your code goes here
-        bill += "Total Price: " + totalPrice + "\n";
+        this.bill += "Total Price: " + this.totalPrice + "\n";
         return this.bill;
     }
 }
