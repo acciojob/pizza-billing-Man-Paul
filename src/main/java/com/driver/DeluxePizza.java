2908e2b29132a780c3e0fe6134ab388388cfaa69
Manoj Paul <manojpaul111@gmail.com>
2023-04-19 23:22:26 +0530

    Pizza Assignment

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -3,6 +3,7 @@ package com.driver;
 public class DeluxePizza extends Pizza {
 
 //    public DeluxePizza()
+//    public DeluxePizza()
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
